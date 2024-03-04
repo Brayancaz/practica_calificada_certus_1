d7c6da73ad31aa29b6a98aeccf9489735214366d
Brayancaz <brayan.ycaza@gmail.com>
2024-03-04 01:30:17 -0500

    actualizando y mejorando las validaciones en js de registro

--- js/formulario_registrate.js
+++ js/formulario_registrate.js
@@ -1,26 +1,19 @@
+// Obtención de elementos del DOM
 const formulario = document.getElementById('formulario_registro');
 const inputs = document.querySelectorAll('#formulario_registro input');
 const selects = document.querySelectorAll('#formulario_registro select');
 
-// const expresiones = {
-//     nombre: /^.{1,5}$/,
-//     apellido: /^.{4,40}$/,
-//     email: /^.{4,40}$/,
-//     cel: /^.[0-9.]{1,9}$/,
-//     dire: /^.{4,150}$/,
-//     contra: /^.{2,8}$/ 
-// }
-
+// Expresiones regulares para validar cada campo del formulario
 const expresiones = {
-    nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Permite letras y espacios, de 1 a 40 caracteres
-    apellido: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Permite letras y espacios, de 1 a 40 caracteres
-    email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/, // Expresión para validar email
+    nombre: /^\b[A-Za-zÁáÉéÍíÓóÚúÜüÑñ][A-Za-zÁáÉéÍíÓóÚúÜüÑñ\s]*\b$/, // Permite registro de nombre
+    apellido: /^[A-Za-zÁáÉéÍíÓóÚúÜüÑñ]{1,}([\s][A-Za-zÁáÉéÍíÓóÚúÜüÑñ]{1,})+$/, // Permite letras y espacios, 2 palabras
+    email: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/, // Expresión para validar email
     cel: /^[0-9]{9}$/, // Permite 9 dígitos numéricos
     dire: /^.{1,150}$/, // Permite cualquier caracter, de 1 a 150 caracteres
-    contra: /^.{1,8}$/ // Permite cualquier caracter, de 1 a 8 caracteres
+    contra: /.*/ ///^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{3,8}$/ // Permite alfanumerico minimo de 3 y max de 8
 }
 
-
+// Objeto para mantener el estado de validación de cada campo del formulario
 const campos = {
     nombre: false,
     apellido: false,
@@ -30,92 +23,63 @@ const campos = {
     contra: false
 }
 
-// const validarCampo = (expresion, input, campo) => {
-//     if (expresion.test(input.value)){
-//         document.getElementById(`${campo}`).classList.add('is-valid'); /*apostrofe inveritod alt + 96*/
-//         document.getElementById(`${campo}`).classList.remove('is-invalid');
-//         campos[campo] = true;
-//     }
-//     else{
-//         document.getElementById(`${campo}`).classList.add('is-valid'); /*apostrofe inveritod alt + 96*/
-//         document.getElementById(`${campo}`).classList.remove('is-invalid');
-//         campos[campo] = false;
-//     }
-// }
-
+// Funcion para validar un campo especifico del formulario
 const validarCampo = (expresion, input, campo) => {
     const feedback = document.getElementById(`${campo}-feedback`);
 
-    if (expresion.test(input.value)) {
-        input.classList.remove('is-invalid');
-        input.classList.add('is-valid');
-        feedback.style.display = 'none'; // Oculta el mensaje de error
-        campos[campo] = true;
-    } else {
-        input.classList.remove('is-valid');
-        input.classList.add('is-invalid');
-        feedback.style.display = 'block'; // Muestra el mensaje de error
-        campos[campo] = false;
+    // Verificar si el elemento feedback existe
+    if (feedback) {
+        if (expresion && expresion.test(input.value)) {
+            input.classList.remove('is-invalid');
+            input.classList.add('is-valid');
+            feedback.style.display = 'none'; // Oculta el mensaje de error
+            campos[campo] = true;
+            console.log(`Campo ${campo} válido`);
+        } else {
+            input.classList.remove('is-valid');
+            input.classList.add('is-invalid');
+            feedback.style.display = 'block'; // Muestra el mensaje de error
+            campos[campo] = false;
+            console.log(`Campo ${campo} inválido`);
+        }
     }
 };
 
-const validarFormulario = () => {
-    let formularioValido = true;
 
-    for (const campo in campos) {
-        if (!campos[campo]) {
-            formularioValido = false;
-            break; // Si encuentra un campo inválido, detiene la validación
+// Función para validar el formulario completo
+ const validarFormulario = () => {
+     let formularioValido = true;
+
+     for (const campo in campos) {
+         if (!campos[campo]) {
+             formularioValido = false;
+             console.log(`Campo ${campo} inválido`);
+             break; // Si encuentra un campo inválido, detiene la validación
+        }else {
+            console.log(`Campo ${campo} válido`);
         }
+
     }
 
     return formularioValido;
-};
+ };
 
-
-
+// Event listener para el evento de envío del formulario
 formulario.addEventListener('submit', (e) => {
-    e.preventDefault(); // Evita que el formulario se envíe por defecto
+    e.preventDefault(); // Evita que el formulario se envie por defecto
 
-    // Llama a una función para validar el formulario
+    // Llama a una funcion para validar el formulario
     if (validarFormulario()) {
-        // Si el formulario es válido, puedes enviarlo aquí o realizar otras acciones
-        formulario.submit(); // Envia el formulario
+        // Si el formulario es valido, se puede enviar aqui o realizar otras acciones
+        // formulario.submit(); // Envia el formulario
+        console.log('Formulario válido, se puede enviar');
+        alert ('Datos validos')
     } else {
-        // Si el formulario no es válido, puedes mostrar mensajes de error o realizar otras acciones
+        // Si el formulario no es valido, se puede mostrar mensajes de error o realizar otras acciones
         console.log('Formulario inválido');
+        alert("complete la informacion")
     }
 });
-
-// const validarFormulario = (e) => {
-//     switch(e.target.name) {
-//         case "nombre":
-//             validarCampo(expresiones.nombre,e.target,'nombre');
-//         break;
-//         case "apellido":
-//             validarCampo(expresiones.apellido,e.target,'apellido');
-//         break;
-//         case "email":
-//             validarCampo(expresiones.email,e.target,'email');
-//         break;
-//         case "cel":
-//             validarCampo(expresiones.cel,e.target,'cel');
-//         break;
-//         case "dire":
-//             validarCampo(expresiones.dire,e.target,'dire');
-//         break;
-//         case "contraseña":
-//             validarCampo(expresiones.contraseña,e.target,'contraseña');
-//         break;
-//     }
-
-// }
-
-// inputs.forEach((input) =>{
-//     input.addEventListener('keyup',validarFormulario);
-//     input.addEventListener('blur',validarFormulario)
-// });
-
 
 inputs.forEach((input) =>{
     input.addEventListener('keyup', () => {
@@ -124,8 +88,8 @@ inputs.forEach((input) =>{
     });
     input.addEventListener('blur', () => {
         const campo = input.getAttribute('name');
-        validarCampo(expresiones[campo], input, campo);
+        if (input.value.trim() !== '') { // Validar solo si el campo no está vacío
+            validarCampo(expresiones[campo], input, campo);
+        }
     });
 });
-
-
